1274de4ed1490727d24bf8c4e08985dc6ee8209c
Prakhar Agarwal <46771542+Prakhar4444@users.noreply.github.com>
2020-05-16 22:55:14 +0530

    Working with numbers and different functions

---
+++ Python/numbers.py
@@ -0,0 +1,13 @@
+from math import *
+num = -50
+print(str(num) + "My favourite number")
+print(abs(num))
+print(pow(4,2))
+print(max(20,30))
+print(min(20,num))
+print(round(3.2))
+print(round(3.5))
+print(round(3.49))
+print(floor(3.8))
+print(ceil(3.1))
+print(sqrt(81))
