e08bff041f76c62558ff02a145921fb97899fc4d
Prakhar Agarwal <46771542+Prakhar4444@users.noreply.github.com>
2020-05-18 23:12:10 +0530

    Function And Return Statements

---
+++ Python/Functions.py
@@ -0,0 +1,4 @@
+def output (name,age):
+    print("Hello "+name+", You are "+age)
+
+output("Prakhar","19")
---
+++ Python/return.py
@@ -0,0 +1,4 @@
+def Cube(number):
+    return pow(number,3)
+    print("Does not Print this line because return statement works as break, and break the function after execution")
+print(Cube(3))
