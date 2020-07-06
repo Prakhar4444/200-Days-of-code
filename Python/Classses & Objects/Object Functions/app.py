4622221a4ef7d5bee878bcf265d1c3b7733b128d
Prakhar Agarwal <46771542+Prakhar4444@users.noreply.github.com>
2020-07-06 21:58:51 +0530

    Add files via upload

---
+++ Python/Classses & Objects/Object Functions/app.py
@@ -0,0 +1,7 @@
+from Student import Student
+
+Student1 = Student("Prakhar","Engineering",8.4,"False")
+Student2 = Student("Sanjeev","Business",9.0,"True")
+
+print(Student1.on_honor_roll())
+print(Student2.on_honor_roll())
